4f33049ae2a6a8c8442079a89b9ec0802168a642
Paradoru <LicLexa@yandex.ru>
2021-12-24 10:39:34 +0200

    Добавьте файлы проекта.

---
+++ 5.1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите x: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Введите y: ");
+            double y = double.Parse(Console.ReadLine());
+            double z = 0;
+            z += Program.Sin(x, y);
+            Console.WriteLine("{0:F2}", z);
+        }
+        static double Sin(double a, double s)
+        {
+            double z = 0;
+            double x = Math.Sin(a);
+            double y = Math.Sin(s);
+            if (a < 0)
+            {
+                x = -1;
+            }
+            if (a == 0)
+            {
+                x = 0;
+            }
+            if (a > 0)
+            {
+                x = 1;
+            }
+            if (s < 0)
+            {
+                y = -1;
+            }
+            if (s == 0)
+            {
+                y = 0;
+            }
+            if (s > 0)
+            {
+                y = 1;
+            }
+            z += x + y;
+            return z;
+        }
+    }
+}
